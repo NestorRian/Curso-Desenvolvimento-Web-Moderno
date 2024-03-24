d62e62c6ec9c005ac2c22b3de6646fa7719381e8
Nestor Rian <nestor.rian2004@gmai.com>
2024-03-24 11:36:35 -0300

    Exercicios

---
+++ funcao/thisEBind02.js
@@ -0,0 +1,9 @@
+function pessoa() {
+    this.idade = 0
+
+    // a funcao setInterval dispara outra funcao a partir de um determinado intervalo que voce passou.
+    setInterval(function() {
+        this.idade++
+        console.log(this.idade)
+    }, 1000)
+}
