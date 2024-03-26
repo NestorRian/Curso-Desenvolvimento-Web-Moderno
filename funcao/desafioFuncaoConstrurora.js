a8f84d26268afb53432a4773b68de6b5bd35b979
NestorRian <nestor.rian2004@gmail.com>
2024-03-26 19:46:41 -0300

    exercícios

--- funcao/desafioFuncaoConstrurora.js
+++ funcao/desafioFuncaoConstrurora.js
@@ -7,4 +7,5 @@ function pessoa(nome) {
 }
 
 const p1 = new pessoa('Nestor')
+
 p1.falar()
---
+++ funcao/iife.js
@@ -0,0 +1,6 @@
+// IIFE -> immediately Invoked Function Expression
+
+(function () {
+    console.log('Será executado na hora!')
+    console.log('Foga do escopo global!')
+})()
