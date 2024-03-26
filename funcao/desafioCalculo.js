9682dfb9329ca4b826a5fedc662357787238ab48
NestorRian <nestor.rian2004@gmail.com>
2024-03-26 20:38:11 -0300

    exercícios

---
+++ funcao/desafioCalculo.js
@@ -0,0 +1,8 @@
+function soma(x, y) {
+    console.log('Valor da Multiplicação é:', x * y)
+    console.log('Valor da soma é:', x + y)
+    console.log('Valor da Subtração é:', x - y)
+    console.log('Valor da Divisão é:', x / y)
+}
+
+soma(10, 5)
