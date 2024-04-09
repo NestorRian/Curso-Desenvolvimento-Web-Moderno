ab72eb6b1d73e9eae9f9d49fcf1e68a4249a3ba8
NestorRian <nestor.rian2004@gmail.com>
2024-04-08 21:19:20 -0300

    exercicios

---
+++ array/map01.js
@@ -0,0 +1,18 @@
+const num = [1, 2, 3, 4, 5]
+
+// map() é um for com propósito
+
+let resultado1 = num.map(function(elemento) {
+    return elemento.toString()
+})
+
+let resultado2 = num.map(function(elemento) {
+    return elemento * 2
+})
+
+console.log(resultado1)
+console.log(resultado2)
+
+const soma10 = elemento => elemento + 10
+const triplo = elemento => elemento * 3
+const FormatParaReais = elemento => `R$ ${parseFloat(elemento).toFixed(2).replace('.', ',')}`
