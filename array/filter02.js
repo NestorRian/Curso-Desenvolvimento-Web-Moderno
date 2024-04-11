e022ad68850275a2b56d62f3dd9c989497a5b2d3
NestorRian <nestor.rian2004@gmail.com>
2024-04-10 22:50:10 -0300

    exercicios

--- array/filter02.js
+++ array/filter02.js
@@ -12,7 +12,8 @@ const produtos = [
     { nome: 'Notebook', preco: 2500, fragil: true},
     { nome: 'Ipad Pro', preco: 3199, fragil: true},
     { nome: 'Copo de Vidro', preco: 12.99, fragil: true},
-    { nome: 'Copo de Plastico', preco: 3.99, fragil: false}
+    { nome: 'Copo de Plastico', preco: 3.99, fragil: false},
+    { nome: 'Iphone', preco: 5000, fragil: true}
 ]
 
 
