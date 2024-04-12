aed4bdad4cdda3d107be0f72a2af3052bbaa538a
NestorRian <nestor.rian2004@gmail.com>
2024-04-11 22:07:37 -0300

    exercicios

--- array/filter03.js
+++ array/filter03.js
@@ -1,11 +1,13 @@
 const produtos = [
-    { nome: 'Notebook', preco: 2500, fragil: true, modelo: 2020},
-    { nome: 'Ipad Pro', preco: 3199, fragil: true, modelo: 2029},
-    { nome: 'Copo de Vidro', preco: 12.99, fragil: true, modelo: 2020},
-    { nome: 'Copo de Plastico', preco: 3.99, fragil: false, modelo: 2019},
-    { nome: 'Iphone', preco: 5000, fragil: true, modelo: 2010}
+    { nome: 'Notebook', preco: 2500, fragil: true, modelo: 2020, novo: false},
+    { nome: 'Ipad Pro', preco: 3199, fragil: true, modelo: 2029, novo: true},
+    { nome: 'Copo de Vidro', preco: 12.99, fragil: true, modelo: 2020, novo: false},
+    { nome: 'Copo de Plastico', preco: 3.99, fragil: false, modelo: 2019, novo: true},
+    { nome: 'Iphone', preco: 5000, fragil: true, modelo: 2010, novo: false}
 ]
 
-const modelo = produto => produto.modelo == 2020
+const modelo = produtos => produtos.modelo == 2020
+const novo = produtos => produtos.novo == true
 
 console.log(produtos.filter(modelo))
+console.log(produtos.filter(novo))
