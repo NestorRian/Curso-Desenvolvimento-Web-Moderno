989dfd6aba5f28905eed48d3d3d6230c2aefa459
NestorRian <nestor.rian2004@gmail.com>
2024-04-25 21:08:47 -0300

    exercicios

---
+++ node/projeto/src/servidor.js
@@ -0,0 +1,29 @@
+const porta = 2002
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos)
+})
+
+app.get('/produtos/:id', (res, req, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // Converte esse obejto para JSON
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor está executando na porta ${porta}.`)
+})
