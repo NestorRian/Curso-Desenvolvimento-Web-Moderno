7a1eac15889393cc727fd066a8e0e821e0a37206
NestorRian <nestor.rian2004@gmail.com>
2024-04-27 21:23:30 -0300

    exercicios

---
+++ esnext/revisao03.js
@@ -0,0 +1,24 @@
+// Object 
+const obj = { a: 1, b: 2, c: 3}
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+// Melhorias na Notação Literal
+const nome = 'Carla'
+const pessoa = {
+    nome,
+    ola() {
+        return 'Olá pessoal!'
+    }
+}
+console.log(pessoa.nome, pessoa.ola())
+
+// Class
+class Animal {}
+class Cachorro extends Animal {
+    falar() {
+        return 'Au Au'
+    }
+}
+
+console.log(new Cachorro().falar())
