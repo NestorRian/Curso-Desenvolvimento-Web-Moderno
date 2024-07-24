0b4e2c833fc5aa392dc5e7b80faebd9607ab2a58
Nestor <nestor.rian2004@gmail.com>
2024-07-24 20:59:22 -0300

    UPDATE

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import BomDia from './componentes/BomDia'
 
-const elemento = <h1>React 2</h1>
-
-ReactDOM.render(elemento, document.getElementById('root'))
+ReactDOM.render(<BomDia nome="Nestor"/>, document.getElementById('root'))
